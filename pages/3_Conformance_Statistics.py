28df21ccc89592ca9560b9b9ce0f37c944c3699a
Rebecca Lee <rebeccalee1428@gmail.com>
2026-01-20 16:05:51 -0500

    Added streamlit conformance statistics tab

---
+++ pages/3_Conformance_Statistics.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Conformance Statistics", layout="wide")
+st.title("Conformance Statistics")
+
+df_raw = st.session_state.get("df_raw")
+mapping = st.session_state.get("mapping")
+
+if df_raw is None:
+    st.info("Go to Home and upload a CSV first.")
+    st.stop()
+
+if mapping is None:
+    st.info("Go to Upload Data and save a mapping first.")
+    st.stop()
+
